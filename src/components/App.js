b5531f7b2702061aa3a2654505b909173b22ceeb
codeiwan <pewlv7976@gmail.com>
2025-01-12 11:25:25 +0900

    chore: jsconfig.json 추가 절대경로 사용 - useState 훅을 App.js에서 관리 - footer 엘리먼트 추가 - jsconfig.json 파일로 절대경로 적용 - 컴포넌트의 import문 경로 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import AppRouter from "./Router";
+import { useState } from "react";
+import AppRouter from "components/Router";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <AppRouter />
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} IWChat</footer>
+    </>
   );
 }
 
