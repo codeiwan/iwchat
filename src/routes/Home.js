350a93910d367e9c4c3f24c7fb2f8c8f7618a7db
codeiwan <pewlv7976@gmail.com>
2025-01-12 17:46:27 +0900

    feat: chat 등록 폼 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,32 @@
-const Home = () => <span>Home</span>
+import { useState } from "react";
+
+const Home = () => {
+  const [chat, setChat] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setChat(value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={chat}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="Chat" />
+    </form>
+  );
+};
 
 export default Home;
