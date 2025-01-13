9ec695d760e40b5b865c3aa1f0e5645638f7b2b9
codeiwan <pewlv7976@gmail.com>
2025-01-14 01:52:52 +0900

    feat: 버튼에 삭제 기능 추가: - window.confirm이 반환하는 값으로 삭제 기능 구현 - deleteDoc을 통한 삭제 기능 구현

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,23 @@
+import { dbService } from "fbase";
+import { doc, deleteDoc } from "firebase/firestore";
+
 const Chat = ({ chatObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
+    console.log(ok);
+    if (ok) {
+      console.log(chatObj.id);
+      const data = await deleteDoc(doc(dbService, "chats", chatObj.id));
+      console.log(data);
+    }
+  };
+
   return (
     <div>
       <h4>{chatObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Chat</button>
+          <button onClick={onDeleteClick}>Delete Chat</button>
           <button>Edit Chat</button>
         </>
       )}
