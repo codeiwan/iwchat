1216737cdb3385427d6cacc6d9f8f39df50bbb5e
codeiwan <pewlv7976@gmail.com>
2025-01-17 04:02:09 +0900

    feat: 자신이 작성한 chat 필터링 기능 구현: - EditProfile.js 파일 삭제 - Profile 컴포넌트에 userObj 프롭스 전달 - 쿼리 함수 where과 orderBy를 통해 필터링과 정렬

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,31 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { collection, orderBy, query, where, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate();
 
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
   };
+
+  const getMyChats = async () => {
+    const chats = await getDocs(
+      query(
+        collection(dbService, "chats"),
+        where("creatorId", "==", userObj.uid),
+        orderBy("createdAt", "asc")
+      )
+    );
+
+    console.log(chats.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyChats();
+  }, []);
 
   return (
     <>
