ef33bb1eb9f21a7eeed8cd19fb09bdaabe261c39
codeiwan <pewlv7976@gmail.com>
2025-01-18 15:07:43 +0900

    feat: 내비게이션의 displayName 업데이트 기능 추가: - Navigation 컴포넌트에 userObj 프롭스 사용 - updateProfile을 통한 userObj의 displayName 수정

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,6 @@
 import { authService, dbService } from "fbase";
 import { collection, orderBy, query, where, onSnapshot } from "firebase/firestore";
+import { updateProfile } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Chat from "components/Chat";
@@ -7,10 +8,25 @@ import Chat from "components/Chat";
 const Profile = ({ userObj }) => {
   const navigate = useNavigate();
   const [chats, setChats] = useState([]);
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName || "");
 
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await updateProfile(userObj, { displayName: newDisplayName });
+    }
   };
 
   useEffect(() => {
@@ -32,6 +48,15 @@ const Profile = ({ userObj }) => {
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
       <div>
         {chats.map((chat) => (
