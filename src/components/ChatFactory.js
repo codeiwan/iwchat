7b26ea775e309c27983eaea1833ebd2609cf014c
codeiwan <pewlv7976@gmail.com>
2025-01-21 01:53:50 +0900

    refactor: Home 컴포넌트 나누고 정리 - ChatFactory 컴포넌트에 Home 컴포넌트의 form 엘리먼트를 이동

---
+++ src/components/ChatFactory.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { dbService, storageService } from "fbase";
+import { collection, addDoc } from "firebase/firestore";
+import { ref, uploadString, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from 'uuid';
+
+const ChatFactory = ({ userObj }) => {
+  const [chat, setChat] = useState("");
+  const [attachment, setAttachment] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+      const response = await uploadString(attachmentRef, attachment, "data_url");
+      attachmentUrl = await getDownloadURL(response.ref);
+    }
+    await addDoc(collection(dbService, "chats"), {
+      text: chat,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
+    });
+    setChat("");
+    setAttachment("");
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setChat(value);
+  };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => setAttachment("");
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={chat}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="file" accept="image/*" onChange={onFileChange} />
+      <input type="submit" value="Chat" />
+      {attachment && (
+        <div>
+          <img src={attachment} width="50px" height="50px" />
+          <button onClick={onClearAttachment}>Clear</button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default ChatFactory;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,11 @@
-import { dbService, storageService } from "fbase";
+import { dbService } from "fbase";
 import { useEffect, useState } from "react";
-import { collection, addDoc, onSnapshot, orderBy, query } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import Chat from "components/Chat";
-import { ref, uploadString, getDownloadURL } from "firebase/storage";
-import { v4 as uuidv4 } from 'uuid';
+import ChatFactory from "components/ChatFactory";
 
 const Home = ({ userObj }) => {
-  const [chat, setChat] = useState("");
   const [chats, setChats] = useState([]);
-  const [attachment, setAttachment] = useState("");
 
   useEffect(() => {
     onSnapshot(
@@ -26,68 +23,9 @@ const Home = ({ userObj }) => {
     );
   }, []);
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
-      const response = await uploadString(attachmentRef, attachment, "data_url");
-      attachmentUrl = await getDownloadURL(response.ref);
-    }
-    await addDoc(collection(dbService, "chats"), {
-      text: chat,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    });
-    setChat("");
-    setAttachment("");
-  };
-
-  const onChange = (event) => {
-    event.preventDefault();
-    const {
-      target: { value },
-    } = event;
-    setChat(value);
-  };
-
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-
-  const onClearAttachment = () => setAttachment("");
-
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <input
-          value={chat}
-          onChange={onChange}
-          type="text"
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="Chat" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" height="50px" />
-            <button onClick={onClearAttachment}>Clear</button>
-          </div>
-        )}
-      </form>
+      <ChatFactory userObj={userObj} />
       <div>
         {chats.map((chat) => (
           <Chat
